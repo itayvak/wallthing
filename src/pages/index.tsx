fb78afb496830f0f38fe5a75d120b4aaf9cff04a
itaypoo <itayanima@gmail.com>
2024-12-07 19:16:33 +0200

    clock pos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { useDateTimeStrings } from "@/hooks/useDateTimeStrings"
 import { useRandomPhoto } from "@/hooks/useRandomPhoto"
 import styles from "@/styles/index.module.css"
+import React, { useEffect, useState } from "react"
 
 export default function IndexPage() {
     const {
@@ -11,9 +12,29 @@ export default function IndexPage() {
         nextPhoto
     } = useRandomPhoto()
 
+    const [clockTransformX, setClockTransformX] = useState(50)
+    const [clockTransformY, setClockTransformY] = useState(50)
+
+    useEffect(() => {
+        const moveClock = () => {
+            const x = Math.floor(Math.random() * -100)
+            const y = Math.floor(Math.random() * -100)
+            setClockTransformX(x)
+            setClockTransformY(y)
+        }
+        moveClock()
+        const t = setInterval(moveClock, 1000 * 5)
+        return () => clearInterval(t)
+    }, [])
+
     return (
         <>
-            <div className={styles.dateTimeBox}>
+            <div className={styles.dateTimeBox} 
+                style={{
+                    "--transform-x": `${clockTransformX}%`,
+                    "--transform-y": `${clockTransformY}%`
+                } as React.CSSProperties}
+            >
                 <div>
                     <h1 className={styles.timeText}>{timeString}</h1>
                     <p className={styles.amPmText}>{amPmString}</p>
